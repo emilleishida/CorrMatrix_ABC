a91ae8894f6840bb333804676a93fb5af1ea4f87
Martin Kilbinger <martin.kilbinger@cea.fr>
2017-03-07 10:41:35 +0100

    Restored test_zero.py

--- scripts/cov_matrix_definition/test_zero.py
+++ scripts/cov_matrix_definition/test_zero.py
@@ -5,60 +5,27 @@ import pylab as plt
 # data
 a = 1.0                                                 # angular coefficient
 b = 2.5                                                 # linear coefficient
-sig = 100
+nobs = 1000                                             # number of data points
 
-n           = []
-sigma_ML    = []
-sigma_m1_ML = []
+x1 = uniform.rvs(loc=-100, scale=200, size=nobs)        # exploratory variable
+x1.sort()
 
-#for nobs in range(50, 1000, 50):
-for nobs in range(2, 100, 1):
+y1_err = norm.rvs(loc=0, scale=10, size=nobs)           # noise
 
-    n.append(nobs)                                             # number of data points
+yreal = a * x1 + b
+y1 = yreal + y1_err
+cov = np.diag([100 for i in range(nobs)])
+y2 = multivariate_normal.rvs(mean=yreal, cov=cov)
 
-    x1 = uniform.rvs(loc=-100, scale=200, size=nobs)        # exploratory variable
-    x1.sort()
+# calculate covariance matrix
+cov_est = []
+for i in range(nobs):
+    line = []
+    for j in range(nobs):
+        line.append((y2[i] - yreal[i]) * (y2[j] - yreal[j]))
+    cov_est.append(line)
 
-    y1_err = norm.rvs(loc=0, scale=10, size=nobs)           # noise
-
-    yreal = a * x1 + b
-    y1 = yreal + y1_err
-    cov = np.diag([sig for i in range(nobs)])
-    y2 = multivariate_normal.rvs(mean=yreal, cov=cov)
-
-    # calculate covariance matrix
-    cov_est = []
-    for i in range(nobs):
-        line = []
-        for j in range(nobs):
-            line.append((y2[i] - yreal[i]) * (y2[j] - yreal[j]))
-        cov_est.append(line)
-
-    cov_est = np.array(cov_est)
-
-
-    this_sigma_ML = sum(np.diag(cov_est))/(nobs-1)
-    this_sigma_m1_ML = 1 / this_sigma_ML
-    sigma_ML.append(this_sigma_ML)
-    sigma_m1_ML.append(this_sigma_m1_ML)
-
-    print(nobs, this_sigma_ML, this_sigma_m1_ML)
-
-plt.figure()
-plt.subplot(1, 2, 1)
-plt.plot(n, sigma_ML)
-plt.plot([n[0], n[-1]], [sig, sig], 'r-')
-plt.xlabel('nobs')
-plt.ylabel('sigma ML')
-
-plt.subplot(1, 2, 2)
-plt.plot(n, sigma_m1_ML)
-plt.plot([n[0], n[-1]], [1.0/sig, 1.0/sig], 'r-')
-plt.xlabel('nobs')
-plt.ylabel('inverse sigma ML')
-
-plt.savefig('sigma_ML')
-
+cov_est = np.array(cov_est)
 
 # plot
 plt.figure()
