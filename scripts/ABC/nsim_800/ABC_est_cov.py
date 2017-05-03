e257683e4c253c5a30ba9f90a3d101d3e21f686a
Emille Ishida <emilleishida@gmail.com>
2017-05-03 22:32:55 +0200

    added ABC with estimated covariance

---
+++ scripts/ABC/nsim_800/ABC_est_cov.py
@@ -0,0 +1,84 @@
+from cosmoabc.priors import flat_prior
+from cosmoabc.ABC_sampler import ABC
+from cosmoabc.plots import plot_2p
+from cosmoabc.ABC_functions import read_input
+from toy_model_functions import model, linear_dist
+
+import numpy as np
+from scipy.stats import uniform, multivariate_normal
+
+def get_cov_ML(mean, cov, size):
+
+    y2 = multivariate_normal.rvs(mean=mean, cov=cov, size=size)
+    # y2[:,j] = realisations for j-th data entry
+    # y2[i,:] = data vector for i-th realisation
+
+    # Estimate mean (ML)
+    ymean = np.mean(y2, axis=0)
+
+    # calculate covariance matrix
+    cov_est = np.cov(y2, rowvar=False)
+
+    if size > 1:
+        pass
+    else:
+        cov_est = [[cov_est]]
+
+    return cov_est
+
+
+#user input file
+filename = 'toy_model.input'
+
+
+#read  user input
+Parameters = read_input(filename)
+
+Parameters['xmin'] = float(Parameters['xmin'][0])
+Parameters['xmax'] = float(Parameters['xmax'][0])
+Parameters['a'] = float(Parameters['a'][0])
+Parameters['b'] = float(Parameters['b'][0])
+Parameters['sig'] = float(Parameters['sig'][0])
+Parameters['nobs'] = int(Parameters['nobs'][0])
+Parameters['nsim'] = int(Parameters['nsim'][0])
+
+
+# set functions
+Parameters['simulation_func'] = model
+Parameters['distance_func'] = linear_dist
+
+# construnct 1 instance of exploratory variable
+x = uniform.rvs(loc=Parameters['xmin'], scale=Parameters['xmax'] - Parameters['xmin'], size=Parameters['nobs'])
+x.sort()
+
+# fiducial model
+ytrue = Parameters['a']*x + Parameters['b']
+
+# real covariance matrix
+cov = np.diag([Parameters['sig'] for i in range(Parameters['nobs'])]) 
+
+# generate catalog
+y = multivariate_normal.rvs(mean=ytrue, cov=cov, size=1)
+
+# add to parameter dictionary
+Parameters['dataset1'] = np.array([[x[i], y[i]] for i in range(Parameters['nobs'])])
+
+
+#estimated covariance matrix
+cov_est = get_cov_ML(ytrue, cov, Parameters['nsim'])
+
+# add covariance to user input parameters
+Parameters['cov'] = cov_est
+
+
+#initiate ABC sampler
+sampler_ABC = ABC(params=Parameters)
+
+#build first particle system
+sys1 = sampler_ABC.BuildFirstPSystem()
+
+#update particle system until convergence
+sampler_ABC.fullABC()
+
+#plot results
+plot_2p( sampler_ABC.T, 'results_correct_cov.pdf' , Parameters)
