9122ac5bf44cb5ac5a7b88633579b676e0861895
Emille Ishida <emilleishida@gmail.com>
2017-05-04 15:49:06 +0200

    correct bug in ABC run

--- scripts/ABC/correct_covariance/ABC_est_cov.py
+++ scripts/ABC/correct_covariance/ABC_est_cov.py
@@ -80,5 +80,39 @@ sys1 = sampler_ABC.BuildFirstPSystem()
 #update particle system until convergence
 sampler_ABC.fullABC()
 
+
+# calculate numerical results
+op1 = open(Parameters['file_root'] + str(sampler_ABC.T) + '.dat', 'r')
+lin1 = op1.readlines()
+op1.close()
+
+data1 = [elem.split() for elem in lin1]
+
+a_samples = np.array([float(line[0]) for line in data1[1:]])
+b_samples = np.array([float(line[1]) for line in data1[1:]])
+
+weights = np.loadtxt(Parameters['file_root'] + str(sampler_ABC.T) + 'weights.dat')
+
+a_mean = sum([a_samples[i] * weights[i] for i in range(sampler_ABC.M)])
+
+nonzerow = sum([1 for item in weights if item > 0])
+a_std = sum([weights[i]*(a_samples[i] - a_mean)/(((nonzerow - 1)/float(nonzerow))*sum(weights))])
+
+b_mean = sum([b_samples[i] * weights[i] for i in range(sampler_ABC.M)])
+b_std = sum([weights[i]*(b_samples[i] - b_mean)/(((nonzerow - 1)/float(nonzerow))*sum(weights))])
+
+# store numerical results
+op2 = open('num_res_corr_cov.dat', 'w')
+op2.write('a_mean    ' + str(a_mean) + '\n')
+op2.write('a_std     ' + str(a_std) + '\n\n\n')
+op2.write('b_mean    ' + str(b_mean) + '\n')
+op2.write('b_std     ' + str(b_std))
+op2.close()
+
+print 'Numerical results:'
+print 'a:    ' + str(a_mean) + ' +- ' + str(a_std)
+print 'b:    ' + str(b_mean) + ' +- ' + str(b_std)
+
+
 #plot results
 plot_2p( sampler_ABC.T, 'results_correct_cov.pdf' , Parameters)
