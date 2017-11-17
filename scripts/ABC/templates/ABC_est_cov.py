a4c4df43f4b2d73310229bda4895f2b61fd22c2b
Emille Ishida <emilleishida@gmail.com>
2017-11-17 16:23:21 +0100

    fix model

--- scripts/ABC/templates/ABC_est_cov.py
+++ scripts/ABC/templates/ABC_est_cov.py
@@ -54,7 +54,7 @@ Parameters['sig'] = float(Parameters['sig'][0])
 Parameters['nobs'] = int(Parameters['nobs'][0])
 
 # set functions
-Parameters['simulation_func'] = model
+Parameters['simulation_func'] = model_cov
 Parameters['distance_func'] = linear_dist
 Parameters['prior']['a']['func'] = gaussian_prior
 Parameters['prior']['b']['func'] = gaussian_prior
