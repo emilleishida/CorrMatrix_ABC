00b6d88c87d90fbbc75ef7b36a24fc988dd6896b
Martin Kilbinger <martin.kilbinger@cea.fr>
2018-05-07 14:35:29 +0200

    Added WL run set-up script

---
+++ scripts/ABC/WL/set_up_wl_run.sh
@@ -0,0 +1,31 @@
+#!/bin/env bash
+
+base_dir="$HOME/astro/ABC/CorrMatrix_ABC/scripts/ABC/WL"
+
+
+files=(cosmo.par cosmo_lens.par nofz_Euclid_1bin.par nofz_Euclid_1bin.txt)
+n=${#files[@]}
+
+arg="$1"
+case "$arg" in
+	"ABC")
+		files{[$n]}=wl_functions.py
+		;;
+	"PMC")
+		;;
+	*)
+		echo "Invalid mode, one of [ABC|PMC]"
+		exit 1
+esac
+
+
+# Set links
+for f in ${files[*]}; do
+	source=$base_dir/$f
+	ln -sf $source
+done
+
+mv nofz_Euclid_1bin.par nofz.par
+
+
+
