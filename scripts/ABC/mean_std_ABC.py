a858ca81b24702088e114be4f5ce0044fc5a5687
Martin Kilbinger <martin.kilbinger@cea.fr>
2018-07-13 09:49:14 +0200

    job_ABC: added option --recov_iter, allows to recompute cov before each iteration

---
+++ scripts/ABC/mean_std_ABC.py
@@ -0,0 +1,71 @@
+#!/bin/env python
+
+import sys
+import numpy as np
+
+from cosmoabc.plots import plot_2p
+from statsmodels.stats.weightstats import DescrStatsW
+
+
+class dummy:
+    def __init__(self, **kwds):
+        self.__dict__.update(kwds)
+
+Parameters = {}
+Parameters['file_root'] = 'linear_PS'
+Parameters['param_to_fit'] = ['a', 'b']
+
+sampler_ABC = dummy()
+print(sys.argv)
+if len(sys.argv) != 2:
+    print('Syntax: mean_std_plot_ABC.py <iteration>')
+    sys.exit(1)
+
+sampler_ABC.T = sys.argv[1]
+
+
+# The following lines are copied from ABC_est_cov.py
+
+def weighted_std(data, weights):
+    """Taken from http://www.itl.nist.gov/div898/software/dataplot/refman2/ch2/weightsd.pdf"""
+
+    mean = np.average(data, weights=weights)
+    c = sum([weights[i] > pow(10, -6) for i in range(weights.shape[0])])
+
+    num = sum([weights[i] * pow(data[i] - mean, 2) for i in range(data.shape[0])])
+    denom = (c - 1) * sum(weights)/float(c)
+
+    return np.sqrt(num / denom)
+
+
+# calculate numerical results
+op1 = open(Parameters['file_root'] + str(sampler_ABC.T) + '.dat', 'r')
+lin1 = op1.readlines()
+op1.close()
+
+data1 = [elem.split() for elem in lin1]
+
+a_samples = np.array([float(line[0]) for line in data1[1:]])
+b_samples = np.array([float(line[1]) for line in data1[1:]])
+
+weights = np.loadtxt(Parameters['file_root'] + str(sampler_ABC.T) + 'weights.dat')
+
+a_results = DescrStatsW(a_samples, weights=weights, ddof=0)
+b_results = DescrStatsW(b_samples, weights=weights, ddof=0)
+
+a_results.std_mean = weighted_std(a_samples, weights)
+b_results.std_mean = weighted_std(b_samples, weights)
+
+# store numerical results
+op2 = open('num_res.dat', 'w')
+op2.write('a_mean    ' + str(a_results.mean) + '\n')
+op2.write('a_std     ' + str(a_results.std_mean) + '\n\n\n')
+op2.write('b_mean    ' + str(b_results.mean) + '\n')
+op2.write('b_std     ' + str(b_results.std_mean) + '\n')
+op2.close()
+
+print 'Numerical results:'
+print 'a:    ' + str(a_results.mean) + ' +- ' + str(a_results.std_mean)
+print 'b:    ' + str(b_results.mean) + ' +- ' + str(b_results.std_mean)
+
+
