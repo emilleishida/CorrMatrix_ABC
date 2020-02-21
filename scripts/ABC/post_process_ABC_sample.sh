3907ccdd611152077b4a30ca8a48312d4fb0ac92
Martin Kilbinger <martin.kilbinger@cea.fr>
2020-02-21 15:29:12 +0100

    post_process/config for tm3 (PMC) plotting

--- scripts/ABC/post_process_ABC_sample.sh
+++ scripts/ABC/post_process_ABC_sample.sh
@@ -1,4 +1,4 @@
-#!/bin/env bash
+#!/usr/bin/env bash
 
 ### Post-process ABC sample, non-WL version (newer)
 
@@ -23,13 +23,13 @@ rm tmp.tmp
 $PMC/bin/ascii2sample.pl -w 0 -t LIN -c '1 2' -s '$par1 $par2' -N 1 ${pref}_PS${num}+weight.dat
 
 # Mean and rms
-#echo "Mean and rms"
-#$PMC/bin/meanvar_sample ${pref}_PS${num}+weight.dat.sample -c ~/astro/repositories/github/CorrMatrix_ABC/scripts/ABC/templates_quad/config_pmc | tee mean_${num}
+echo "Mean and rms"
+$PMC/bin/meanvar_sample ${pref}_PS${num}+weight.dat.sample -c ~/astro/repositories/github/CorrMatrix_ABC/scripts/ABC/templates_quad/config_pmc | tee mean_${num}
 
 # Plotting
 echo "Resample to create sample file"
 Rscript $PMC/R/sample_from_pmcsimu.R -M 2 -i ${pref}_PS${num}+weight.dat.sample -o sample_${num}
 
 echo "1D and 2D plots"
-Rscript $PMC/R/plot_confidence.R sample_${num} -c ~/astro/Runs/ABC/PMC/WL/Euclid_2par_1bin/config_pmc -o pdf --pmin=0.1_0.6 --pmax=0.5_1.0 -N 200 --gsmooth=70 -C 1
+Rscript $PMC/R/plot_confidence.R sample_${num} -c ~/astro/repositories/github/CorrMatrix_ABC/scripts/ABC/templates_quad/config_pmc -o pdf --pmin=0.275_0.8 --pmax=0.335_0.86 -N 200 --gsmooth=70 -C 0 -m 0.306_0.827
 
