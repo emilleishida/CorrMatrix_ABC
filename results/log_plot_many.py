10cfa46aeeb0ecf6e22a32dae5000fe13103c871
martinkilbinger <martin.kilbinger@cea.fr>
2021-10-15 17:52:25 +0200

    plot_many cleaned up

--- results/log_plot_many.py
+++ results/log_plot_many.py
@@ -1,1 +1,1 @@
-../scripts/plot_many.py --ABC norm_ABC/mean_std_ABC --MCMC_norm norm_deb_MCMC/mean_std_fit_norm_deb --MCMC_T2 T2_MCMC/mean_std_fit_SH -b 0.03 --sig_var 4.6e-8_1.75e-4
+../scripts/plot_many.py --ABC norm_ABC/mean_std_ABC --MCMC_norm norm_deb_MCMC/mean_std_fit_norm_deb --MCMC_T2 T2_MCMC/mean_std_fit_SH -b 0.03 --sig_var 4.6e-8_1.75e-4 
